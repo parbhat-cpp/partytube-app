01c71bbc80d2768aed1611a5e6e2dc2a0ab7769f
parbhat-cpp <prabhat29ps@gmail.com>
2024-05-04 13:18:19 +0530

    add loader

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:partytube_app/state_management/socket_manager.dart';
 import 'package:partytube_app/widgets/create_room.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  late bool loader;
+  late SocketManager socketManager;
+
+  @override
+  void initState() {
+    super.initState();
+    initSocket();
+  }
+
+  initSocket() {
+    setState(() {
+      loader = true;
+    });
+
+    socketManager = SocketManager();
+    socketManager.connect(context);
+
+    setState(() {
+      loader = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,21 +40,36 @@ class Homepage extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.black,
         ),
-        body: const Column(
-          children: [
-            ExpansionTile(
-              initiallyExpanded: true,
-              title: Text("Create Room"),
+        body: Builder(builder: (context) {
+          if (loader) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                CreateRoom()
+                Center(
+                  child: LoadingAnimationWidget.prograssiveDots(
+                    color: Colors.black26,
+                    size: 75,
+                  ),
+                ),
+                const Text('Connecting to server...'),
               ],
-            ),
-            ExpansionTile(
-              title: Text("Join Room"),
-              children: [],
-            ),
-          ],
-        ),
+            );
+          } else {
+            return const Column(
+              children: [
+                ExpansionTile(
+                  initiallyExpanded: true,
+                  title: Text("Create Room"),
+                  children: [CreateRoom()],
+                ),
+                ExpansionTile(
+                  title: Text("Join Room"),
+                  children: [],
+                ),
+              ],
+            );
+          }
+        }),
       ),
     );
   }
