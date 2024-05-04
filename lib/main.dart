9b6e06f35394d69dcbaa32e6f28538dc3c0ffdb9
parbhat-cpp <prabhat29ps@gmail.com>
2024-05-04 13:17:34 +0530

    add .env file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:partytube_app/app/my_app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
