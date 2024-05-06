3157ab529bc35ed5f244875e1f7aaadc92f3cec6
parbhat-cpp <prabhat29ps@gmail.com>
2024-05-06 20:21:51 +0530

    use multibloc for state management

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:partytube_app/pages/homepage.dart';
+import 'package:partytube_app/state_management/room_state.dart';
 import 'package:partytube_app/state_management/socket_manager.dart';
 
 class MyApp extends StatelessWidget {
@@ -9,16 +10,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: BlocProvider(
-        child: const Homepage(),
-        create: (_) => SocketManager(),
+    return MultiBlocListener(
+      listeners: [
+        BlocProvider(
+          create: (BuildContext context) => SocketManager(),
+        ),
+        BlocProvider(
+          create: (BuildContext context) => RoomState(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const Homepage(),
       ),
     );
   }
