41ae27b99c5a74c23f67304e6d70d4fe899cf483
parbhat-cpp <prabhat29ps@gmail.com>
2024-05-12 12:07:26 +0530

    using socket manager var

--- lib/pages/room.dart
+++ lib/pages/room.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:partytube_app/model/room.dart';
@@ -23,75 +24,124 @@ class _RoomState extends State<Room> {
   bool userFound = false;
   bool roomFound = false;
 
+  late SocketManager socketManager;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   void initState() {
     super.initState();
+
+    socketManager = context.read<SocketManager>();
 
     setState(() {
       roomId = context.read<RoomState>().getRoomId();
       userId = context.read<RoomState>().getUserId();
     });
 
-    context.read<SocketManager>().socketEmit("get-room", roomId);
-    context.read<SocketManager>().socketEmit("get-user", userId);
+    socketManager.socketEmit("get-room", roomId);
+    socketManager.socketEmit("get-user", userId);
 
-    context.read<SocketManager>().socketListen("user-info", (userJson) {
+    socketManager.socketListen("user-info", (userJson) {
       setState(() {
         user = UserModel.fromJson(userJson);
         userFound = true;
       });
     });
 
-    context.read<SocketManager>().socketListen("room-info", (roomJson) {
+    socketManager.socketListen("room-info", (roomJson) {
       setState(() {
         room = RoomModel.fromJson(roomJson);
         roomFound = true;
       });
     });
 
-    context.read<SocketManager>().socketListen("room-not-found", (p0) {
-      Navigator.of(context).pop(context);
+    socketManager.socketListen("room-not-found", (p0) {
+      Navigator.pop(context);
     });
 
-    context.read<SocketManager>().socketListen("user-not-found", (p0) {
-      Navigator.of(context).pop(context);
+    socketManager.socketListen("user-not-found", (p0) {
+      Navigator.pop(context);
     });
 
-    context.read<SocketManager>().socketListen("admin-left-room", (p0) {
-      ScaffoldMessengerState scaffold = ScaffoldMessenger.of(context);
-      scaffold.showSnackBar(const SnackBar(
+    socketManager.socketListen("admin-left-room", (p0) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
         content: Text('Admin left the room!'),
       ));
 
-      Navigator.of(context).pop(context);
+      Navigator.pop(context);
+    });
+
+    socketManager.socketListen("user-left", (userLeftName) {
+      ScaffoldMessengerState scaffold = ScaffoldMessenger.of(context);
+
+      socketManager.socketEmit("get-room", roomId);
+
+      scaffold.showSnackBar(SnackBar(
+        content: Text('$userLeftName left the room!'),
+      ));
+    });
+
+    socketManager.socketListen("new-user-joined", (username) {
+      ScaffoldMessengerState scaffold = ScaffoldMessenger.of(context);
+
+      socketManager.socketEmit("get-room", roomId);
+
+      scaffold.showSnackBar(SnackBar(
+        content: Text('$username joined the room!'),
+      ));
+    });
+
+    socketManager.socketListen("remove-user-response", (removeUserId) {
+      if (userId == removeUserId) {
+        socketManager.socketEmit("user-removed", user.name);
+
+        Navigator.pop(context);
+      }
+    });
+
+    socketManager.socketListen("user-kicked", (username) {
+      ScaffoldMessengerState scaffold = ScaffoldMessenger.of(context);
+
+      socketManager.socketEmit("get-room", roomId);
+
+      scaffold.showSnackBar(SnackBar(
+        content: Text('$username was removed by the admin'),
+      ));
     });
   }
 
   @override
   void dispose() {
+    socketManager.removeListener("leave-room");
+    socketManager.removeListener("user-left");
+    socketManager.removeListener("admin-left-room");
+    socketManager.removeListener("user-not-found");
+    socketManager.removeListener("room-not-found");
+    socketManager.removeListener("room-info");
+    socketManager.removeListener("user-info");
+    socketManager.removeListener("get-user");
+    socketManager.removeListener("get-room");
+    socketManager.removeListener("new-user-joined");
+    socketManager.removeListener("remove-user");
+    socketManager.removeListener("remove-user-response");
+    socketManager.removeListener("user-removed");
+    socketManager.removeListener("user-kicked");
+
     super.dispose();
-
-    context.read<SocketManager>().removeListener("leave-room");
-    context.read<SocketManager>().removeListener("admin-left-room");
-    context.read<SocketManager>().removeListener("user-not-found");
-    context.read<SocketManager>().removeListener("room-not-found");
-    context.read<SocketManager>().removeListener("room-info");
-    context.read<SocketManager>().removeListener("user-info");
-    context.read<SocketManager>().removeListener("get-user");
-    context.read<SocketManager>().removeListener("get-room");
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
   }
 
   void handleRoomLeave() {
-    context
-        .read<SocketManager>()
-        .socketEmit("leave-room", {user.id, user.name, user.room});
+    socketManager.socketEmit("leave-room", {user.id, user.name, user.room});
 
-    Navigator.of(context).pop(context);
+    Navigator.pop(context);
+  }
+
+  bool isUserAdmin(String userId) {
+    return room.roomAdminId == userId;
+  }
+
+  void removeUser(String userId) {
+    socketManager.socketEmit("remove-user", {userId, roomId});
   }
 
   @override
@@ -104,7 +154,7 @@ class _RoomState extends State<Room> {
             children: [
               Center(
                 child: LoadingAnimationWidget.prograssiveDots(
-                  color: Colors.black26,
+                  color: Colors.blue.shade300,
                   size: 75,
                 ),
               ),
@@ -112,15 +162,47 @@ class _RoomState extends State<Room> {
           );
         } else {
           return Scaffold(
+            key: _scaffoldKey,
+            endDrawer: Drawer(
+              child: ListView.builder(
+                itemCount: room.users.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      leading: Visibility(
+                        visible: isUserAdmin(userId) &&
+                            !isUserAdmin(room.users[index].userId),
+                        child: IconButton(
+                          tooltip: 'Remove user',
+                          onPressed: () => removeUser(room.users[index].userId),
+                          icon: const Icon(Icons.close),
+                        ),
+                      ),
+                      iconColor: Colors.red,
+                      title: Text(
+                          '${room.users[index].username} ${isUserAdmin(room.users[index].userId) ? '(Admin)' : ''}'),
+                    ),
+                  );
+                },
+              ),
+            ),
             appBar: AppBar(
               title: Text('${room.roomName} (${room.roomId})'),
-              backgroundColor: Colors.black,
+              backgroundColor: Colors.blue.shade300,
               automaticallyImplyLeading: false,
               leading: Builder(builder: (BuildContext context) {
                 return IconButton(
                     onPressed: handleRoomLeave,
                     icon: const Icon(Icons.arrow_back));
               }),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    _scaffoldKey.currentState!.openEndDrawer();
+                  },
+                  icon: const Icon(Icons.menu),
+                ),
+              ],
             ),
             body: Column(
               children: [
