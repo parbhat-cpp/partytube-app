040f9b2f0f3f11e330a90a601c15287ca1e232d6
parbhat-cpp <prabhat29ps@gmail.com>
2024-05-25 17:12:15 +0530

    update youtube api service

--- lib/services/youtube_api.dart
+++ lib/services/youtube_api.dart
@@ -30,7 +30,7 @@ class YouTubeApi {
 
   Future<void> search(String searchQuery, bool loadNext) async {
     String searchUrl =
-        '$youtubeApiUrl/search?key=${dotenv.env['API_KEY']}&q=$searchQuery&maxResults=${searchFilter['maxResults']}&type=video';
+        '$youtubeApiUrl/search?key=${dotenv.env['API_KEY']}&q=$searchQuery&maxResults=${searchFilter['maxResults']}&type=video&part=snippet';
 
     if (loadNext) {
       searchUrl = '$searchUrl&pageToken=$nextPageToken';
@@ -47,9 +47,27 @@ class YouTubeApi {
 
       nextPageToken = searchResultJson['nextPageToken'];
 
-      searchResult = [...searchResult, ...searchResultJson['items']];
+      List<dynamic> searchData = searchResultJson['items'];
+      List<Map<String, dynamic>> searchDataList = [];
 
-      // print(searchResult);
+      for (int i = 0; i < searchData.length; i++) {
+        String videoId = searchData[i]['id']['videoId'];
+        String videoTitle = searchData[i]['snippet']['title'];
+        String thumbnailUrl = searchData[i]['snippet']['thumbnails']['default']['url'];
+        int thumbnailWidth = searchData[i]['snippet']['thumbnails']['default']['width'];
+        int thumbnailHeight = searchData[i]['snippet']['thumbnails']['default']['height'];
+
+        searchDataList.add({
+          "videoId": videoId,
+          "title": videoTitle,
+          "thumbnailUrl": thumbnailUrl,
+          "thumbnailWidth": thumbnailWidth.toDouble(),
+          "thumbnailHeight": thumbnailHeight.toDouble()
+        });
+      }
+
+      searchResult = [...searchResult, ...searchDataList];
+
     } else {
       throw Exception('Failed to load data');
     }
